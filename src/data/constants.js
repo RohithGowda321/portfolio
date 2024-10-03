53f085ecd06f2c15543098785eff6896d076184c
RohithGowda321 <160516020+RohithGowda321@users.noreply.github.com>
2024-10-03 16:42:43 +0530

    Add files via upload

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,10 +3,10 @@ export const Bio = {
   roles: ["Front End  Developer", "React JS Developer"],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  // github: "https://github.com/rishavchanda",
-  // resume:
-  //   "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  // linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  github: "https://github.com/Rohith03-09",
+  resume:
+    "https://drive.google.com/file/d/1q1p07eIpfGQ8KUBUSXLAhnPP89m5fnCj/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rohith-gowda-m-m-28a156193",
 };
 
 export const skills = [
